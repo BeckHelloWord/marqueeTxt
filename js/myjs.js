333179638473daf1516ac6faf72fab0d6787eba6
yuwei <shinyhz50817>
2016-05-20 16:10:46 +0800

    无缝滚动-进阶
    无缝滚动-进阶

--- js/myjs.js
+++ js/myjs.js
@@ -1,33 +1,51 @@
 /**
  * Created by yuwei on 2016/5/19.
+ * 注意：当使用过渡效果滚动（way=false）时，需要将interval改为1000或更大的值
  */
+
 (function ($) {
+
     var defaults = {
-        dom: '.box .li-list',
-        interval: 1000
+        interval: 40,
+        way: true    //滚动方式ture为默认滚动，flase为过渡滚动
     };
 
     $.fn.txtRoll = function (options) {
-        var timer = null;
         var options = $.extend(defaults, options);
-        $(options.dom).html($(options.dom).html() + $(options.dom).html());
 
-        var fun = function () {
-            $(options.dom).stop().animate({"margin-top": -$(options.dom).find('li').outerHeight(true)}, 500, function () {
-                $(options.dom).find('li').first().appendTo($(options.dom));
-                $(options.dom).css('margin-top', 0);
-            });
-        }
+        var timer = null, _this = $(this);
+        _this.html(_this.html() + _this.html());
 
-        clearInterval(timer);
-        timer = setInterval(fun, options.interval);
+        //判断复制后内容是否超出盒子高度
+        if (_this.height() > _this.parent().height()) {
+            //滚动
+            var fun = function () {
+                _this.stop().animate({'margin-top': options.way ? '-=1' : -_this.find('li').outerHeight(true)}, options.way ? 0 : 500, function () {
+                    if (options.way) {
+                        //每滚动一次判断一下是否等于一条内容的高度，如果是将第一条移动到最后，外边据清零
+                        var gap = parseInt(_this.css("margin-top"));
+                        if (gap === -_this.find('li').outerHeight(true)) {
+                            _this.find('li').first().appendTo(_this);
+                            _this.css('margin-top', 0);
+                        }
+                    } else {
+                        _this.find('li').first().appendTo(_this);
+                        _this.css('margin-top', 0);
+                    }
+                })
+            }
 
-        $(options.dom).on('mouseover', function () {
+            //定时
             clearInterval(timer);
-        });
-        $(options.dom).on('mouseout', function () {
             timer = setInterval(fun, options.interval);
-        });
+
+            //停止
+            _this.mouseenter(function () {
+                clearInterval(timer);
+            }).mouseleave(function () {
+                timer = setInterval(fun, options.interval);
+            })
+        }
     }
 
 })(jQuery)
